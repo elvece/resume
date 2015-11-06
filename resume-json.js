5a9f20858a2e10b6599b0dc78736aaa2e3fe4b87
elvece <lvcifferello@gmail.com>
2015-11-06 14:55:45 -0700

    added basic info

---
+++ resume-json.js
@@ -0,0 +1,103 @@
+{
+  "basics": {
+    "name": "Lucy Cifferello",
+    "label": "Full-Stack Developer",
+    "picture": "",
+    "email": "lvcifferello@gmail.com",
+    "phone": "(484) 356-5141",
+    "website": "http://elvece.com",
+    "summary": "Hey! I'm a code crusher in the making and am passionate about starting a career as a kickass full-stack web developer. I really enjoy understanding the underlying processes of code, creating new ways to approach problems, and really breaking things down to get into each puzzle piece. I want to be a part of an awesome company where I feel like I am contributing to its growth and success.\nCheck out my LinkedIn for a more in depth description about myself, as I'm keeping this dry.",
+    "location": {
+      "postalCode": "CO",
+      "city": "Denver",
+      "countryCode": "US",
+      "region": "Greater Denver Area"
+    },
+    "profiles": [
+    {
+      "network": "GitHub",
+      "username": "elvece",
+      "url": "https://github.com/elvece"
+    },
+    {
+      "network": "LinkedIn",
+      "username": "lucyvictoria",
+      "url": "https://www.linkedin.com/in/lucyvictoria"
+    },
+    {
+      "network": "Twitter",
+      "username": "el_ve_ce",
+      "url": "https://twitter.com/el_ve_ce"
+    }
+  ]},
+  "work": [{
+    "company": "Company",
+    "position": "President",
+    "website": "http://company.com",
+    "startDate": "2013-01-01",
+    "endDate": "2014-01-01",
+    "summary": "Description...",
+    "highlights": [
+      "Started the company"
+    ]
+  }],
+  "volunteer": [{
+    "organization": "Organization",
+    "position": "Volunteer",
+    "website": "http://organization.com/",
+    "startDate": "2012-01-01",
+    "endDate": "2013-01-01",
+    "summary": "Description...",
+    "highlights": [
+      "Awarded 'Volunteer of the Month'"
+    ]
+  }],
+  "education": [{
+    "institution": "University",
+    "area": "Software Development",
+    "studyType": "Bachelor",
+    "startDate": "2011-01-01",
+    "endDate": "2013-01-01",
+    "gpa": "4.0",
+    "courses": [
+      "DB1101 - Basic SQL"
+    ]
+  }],
+  "awards": [{
+    "title": "Award",
+    "date": "2014-11-01",
+    "awarder": "Company",
+    "summary": "There is no spoon."
+  }],
+  "publications": [{
+    "name": "Publication",
+    "publisher": "Company",
+    "releaseDate": "2014-10-01",
+    "website": "http://publication.com",
+    "summary": "Description..."
+  }],
+  "skills": [{
+    "name": "Web Development",
+    "level": "Master",
+    "keywords": [
+      "HTML",
+      "CSS",
+      "Javascript"
+    ]
+  }],
+  "languages": [{
+    "language": "English",
+    "fluency": "Native speaker"
+  }],
+  "interests": [{
+    "name": "Wildlife",
+    "keywords": [
+      "Ferrets",
+      "Unicorns"
+    ]
+  }],
+  "references": [{
+    "name": "Jane Doe",
+    "reference": "Reference..."
+  }]
+}
